e0ff39171b6621462218c24c5c8eb16c5e5e1da0
Ryan Ellenburg <ryanellenburg@gmail.com>
2024-10-24 20:26:38 -0400

    Main screen fixed. Widget image issue persists. Also need to fix app icon.

--- app/src/main/java/com/synclytic/ringsnoozewidget/MainActivity.kt
+++ app/src/main/java/com/synclytic/ringsnoozewidget/MainActivity.kt
@@ -1,25 +1,13 @@
 package com.synclytic.ringsnoozewidget
 
-import android.appwidget.AppWidgetManager
-import android.content.ComponentName
 import android.os.Bundle
-import android.widget.RemoteViews
 import androidx.appcompat.app.AppCompatActivity
-import com.synclytic.ringsnoozewidget.app.MainApplication
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-
-        // Trigger widget update
-        val appWidgetManager = AppWidgetManager.getInstance(this)
-        val myWidget = ComponentName(this, MainApplication::class.java)
-        appWidgetManager.updateAppWidget(myWidget, RemoteViews(packageName, R.layout.ring_snooze_widget))
-
-        // Finish the activity (since we don't need the activity to stay open)
-        finish()
 
     }
 }
